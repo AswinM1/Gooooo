d8f5dc00b9d895f82fa4f723b341c928af01fb8b
Aswin <117593557+AswinM1@users.noreply.github.com>
2026-09-15 21:47:04 +0530

    ran

--- http/main.go
+++ http/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Message struct {
@@ -23,6 +25,12 @@ func userCreate(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/api/user", userCreate)
-	http.ListenAndServe(":8000", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/api/{name}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		json.NewEncoder(w).Encode(vars["name"])
+
+	}).Methods("GET")
+	http.ListenAndServe(":8000", router)
 
 }
